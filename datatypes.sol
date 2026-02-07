7d91eefd89fa3609ceb2e3aa85ddeb52ed59d2b2
Aarna chauhan <chauhanaarna6@gmail.com>
2026-02-07 11:26:17 +0530

    Create datatypes.sol

---
+++ datatypes.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+contract valuesType{
+    bool public booltemp=true;
+    uint public temp=234;
+    int public tem2=7393;
+    address public addr;
+    bytes32 public b3;
+}
