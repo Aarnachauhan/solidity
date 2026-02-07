450f95d2e6cbe078873dc97618e32c2a0ef5e93a
Aarna chauhan <chauhanaarna6@gmail.com>
2026-02-07 11:26:37 +0530

    Update sample contract.sol

--- sample contract.sol
+++ sample contract.sol
@@ -1,1 +1,25 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ownable{
+    address public owner;
+    constructor(){
+        owner=msg.sender;
+    }
+    modifier onlyowner{
+        require(msg.sender==owner,"not owner");
+        _; 
+    }
+
+    function setOwner(address _newOwner) public onlyowner{
+        require(_newOwner !=address(0),"invalid address");
+        owner=_newOwner;
+    }
+    function onlyowneraccess() public onlyowner{
+        //code
+    }
+    function anyonecanaccess() public{
+        //code
+    }
+}
