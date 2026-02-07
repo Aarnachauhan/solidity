85032483be9179e957928ffd462cae2f4de506e5
Aarna chauhan <chauhanaarna6@gmail.com>
2026-02-07 12:22:27 +0530

    Update variable initialisation.sol

--- variable initialisation.sol
+++ variable initialisation.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract statevariable{
+    //1st way to initialise 
+ uint public salary=1000;
+
+ //2nd way: make a constructor 
+ constructor(){
+    salary=208282;
+ }
+ //3rd :
+ function setSalary() public {
+    salary=192020;
+ }
+
+}
 
