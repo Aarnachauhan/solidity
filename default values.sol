a20e3daee4497cc9e170083b9adffd5315b71faf
Aarna chauhan <chauhanaarna6@gmail.com>
2026-02-09 14:03:10 +0530

    Create default values.sol

---
+++ default values.sol
@@ -0,0 +1,12 @@
+//outputs are in comments.
+
+pragma solidity ^0.8.0;
+
+contract defaultvalues{
+    uint public x; //op 0 
+    bool public y;  //false
+    address public z; //0000...
+    bytes32 public a; //0x0000....
+    string public str; //empty string
+
+}
